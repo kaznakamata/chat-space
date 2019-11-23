e2f55b5c972d0da1377e0e459dd85ec875bdda1b
kaznakamata <kazyokoyama8754@gmail.com>
2019-11-23 12:30:01 +0900

    insert message into html with ajax

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,38 +1,41 @@
 $(function(){
   function buildHTML(message){
     // 「もしメッセージに画像が含まれていたら」という条件式
-    if (message.image) {
-      var html = `<div class="group__chat__contents__info">
-                    <div class="group__chat__contents__info__who">
+    if (message.image == true) {
+      var html = 
+      `<div class="group__contents__info">
+                    <div class="group__contents__info__who">
                       ${message.user_id}
                     </div>
-                    <div class="group__chat__contents__info__date">
+                    <div class="group__contents__info__date">
                       ${message.created_at}
                     </div>
                   </div>
-                  <div class="group__chat__contents__message">
+                  <div class="group__contents__message">
                       ${message.body}
                       <img src="${message.image}">
                   </div>`//メッセージに画像が含まれる場合のHTMLを作る
+      return html;
     } else {
-      var html = `<div class="group__chat__contents__info">
-                    <div class="group__chat__contents__info__who">
+      var html = `<div class="group__contents__info">
+                    <div class="group__contents__info__who">
                       ${message.user_id}
                     </div>
-                    <div class="group__chat__contents__info__date">
+                    <div class="group__contents__info__date">
                       ${message.created_at}
                     </div>
                   </div>
-                  <div class="group__chat__contents__message">
+                  <div class="group__contents__message">
                       ${message.body}
                   </div>
       `//メッセージに画像が含まれない場合のHTMLを作る
+      return html;
     }
   }
-  $('.new_message').on('submit', function(e){
+  $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
-    var url = $(this).attr('action')
+    var url = $(this).attr('action');
     $.ajax({
       url: url,
       type: "POST",
@@ -43,11 +46,9 @@ $(function(){
     })
     .done(function(message){
       var html = buildHTML(message);
-      $('.group__chat').append(html);
-      console.log('どうなったかな');
-    })
-    .fail(function(){
-      alert('error');
+      $('.group__contents').append(html);
+      $('#message_body').val('');
+      $('.group__post__form__btn').prop('disabled', false);
     })
   })
 })
---
+++ app/kensyouyou.js
@@ -0,0 +1,9 @@
+function buildHTML(message){
+  // 「もしメッセージに画像が含まれていたら」という条件式
+  if message.image {
+    var html = //メッセージに画像が含まれる場合のHTMLを作る
+  } else {
+    var html = //メッセージに画像が含まれない場合のHTMLを作る
+  }
+  return html
+}
