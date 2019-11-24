0319ecfa180cce2e9ea4b3d3e93c59aa626c7ad0
kaznakamata <kazyokoyama8754@gmail.com>
2019-11-24 12:14:43 +0900

    ajax-for-sending-messages

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,10 +1,12 @@
 class Group < ApplicationRecord
+  validates :name, uniqueness: true
   has_many :group_users
   has_many :users, through: :group_users
   has_many :messages
   def show_last_message
     if (last_message = messages.last).present?
       last_message.body? ? last_message.body : '画像が投稿されています'
+      binding.pry
     else
       'まだメッセージはありません。'
     end
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -1,4 +1,5 @@
 json.body  @message.body
-json.image @message.image
+json.image @message.image.url
+json.name @message.user.name
 json.user_id  @message.user_id
-json.created_at @message.created_at
+json.created_at @message.created_at.strftime("%Y/%m/%d %H:%M")
