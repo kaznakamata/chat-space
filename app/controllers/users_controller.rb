4860b9055f53ecd5c4b7f5b19f181374b4752eda
kaznakamata <kazyokoyama8754@gmail.com>
2019-11-26 22:20:52 +0900

    finalize of incremental search

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   def index
     @users = User.search(params[:keyword], current_user.id)
     respond_to do |format|
-      format.html 
+      format.html
       format.json
     end
   end
