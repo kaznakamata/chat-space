467b4a314741b0e6a8145cee25e1a3bed5686219
kaznakamata <kazyokoyama8754@gmail.com>
2019-12-03 21:49:08 +0900

    correct views

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,8 +1,8 @@
 class Group < ApplicationRecord
-  validates :name, uniqueness: true
   has_many :group_users
-  has_many :users, through: :group_users
   has_many :messages
+  has_many :users, through: :group_users
+  validates :name, uniqueness: true
   def show_last_message
     if (last_message = messages.last).present?
       last_message.body? ? last_message.body : '画像が投稿されています'
